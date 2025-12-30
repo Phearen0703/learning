fbb168614badc75e11e9c4d51433b9201144ae48
Phearen <phearen0703@gmail.com>
2025-12-30 10:29:34 +0700

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
+import Home from "./student/pages/Home";
+import Login from "./auth/Login";
+
 import AdminLayout from "./admin/layout/AdminLayout";
 import Dashboard from "./admin/pages/Dashboard";
 import Courses from "./admin/pages/Courses";
 import Users from "./admin/pages/Users";
+import Roles from "./admin/pages/settings/Roles";
+import Permissions from "./admin/pages/settings/Permissions";
 
 import AdminRoute from "./routes/AdminRoute";
 import PermissionRoute from "./routes/PermissionRoute";
@@ -13,6 +18,12 @@ function App() {
   return (
     <AuthProvider>
       <Routes>
+
+        {/* STUDENT HOME */}
+        <Route path="/" element={<Home />} />
+
+        {/* LOGIN */}
+        <Route path="/login" element={<Login />} />
 
         {/* ADMIN */}
         <Route
@@ -44,26 +55,23 @@ function App() {
           />
 
           <Route
-          path="/admin/settings/roles"
-          element={
-            <PermissionRoute permission="manage_roles">
-              <Roles />
-            </PermissionRoute>
-          }
-        />
+            path="settings/roles"
+            element={
+              <PermissionRoute permission="manage_roles">
+                <Roles />
+              </PermissionRoute>
+            }
+          />
 
-        <Route
-          path="/admin/settings/permissions"
-          element={
-            <PermissionRoute permission="manage_roles">
-              <Permissions />
-            </PermissionRoute>
-          }
-        />
+          <Route
+            path="settings/permissions"
+            element={
+              <PermissionRoute permission="manage_roles">
+                <Permissions />
+              </PermissionRoute>
+            }
+          />
         </Route>
-
-        
-
 
       </Routes>
     </AuthProvider>
